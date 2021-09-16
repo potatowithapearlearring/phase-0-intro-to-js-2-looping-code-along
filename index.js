c8e436df7b3f0e7b6a98289d608379bda1690f30
Avery Lin <tclin100@gmail.com>
2021-09-15 22:25:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const name = ['Lisa', 'Kaitlin', 'Jan',]
+const event = 'surprise'
+
+
+function writeCards(name, event) {
+    const newArr = [];
+    for (let i = 0 ; i <name.length; i++) {
+        newArr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return newArr;
+}
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >=0) {
+        console.log(init--);
+    }
+} 
